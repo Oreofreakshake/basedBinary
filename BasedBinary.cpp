69fbb14c393c4f2e42b8b6a4934fb8827ac0e96d
Yazak <53213763+Oreofreakshake@users.noreply.github.com>
2021-11-03 15:57:57 +0500

    added hex

--- BasedBinary.cpp
+++ BasedBinary.cpp
@@ -15,38 +15,93 @@ void BaseTenToTwo(int NumYouEnter){
         NumYouEnter >>= 1;
     }
     
-    cout << "binary value = " << bin << "\n";
+    cout << "\nbinary value = " << bin << "\n";
 }
 
 
-void BaseSixteenToTwo(int NumYouEnter){
-    
-    string bin{};
-    
-    while( NumYouEnter > 0 ) {
-        if( NumYouEnter % 16 == 0 ) bin.insert( bin.begin( ), '0' );
-        else bin.insert( bin.begin( ), '1' );
-        
-        NumYouEnter >>= 1;
+void BaseSixteenToTwo(char NumYouEnter[10]){
+	
+    int i=0;
+
+    cout<<"\nbinary Value = ";
+    while(NumYouEnter[i])
+    {
+        switch(NumYouEnter[i])
+        {
+            case '0':
+                cout<<"0000";
+                break;
+            case '1':
+                cout<<"0001";
+                break;
+            case '2':
+                cout<<"0010";
+                break;
+            case '3':
+                cout<<"0011";
+                break;
+            case '4':
+                cout<<"0100";
+                break;
+            case '5':
+                cout<<"0101";
+                break;
+            case '6':
+                cout<<"0110";
+                break;
+            case '7':
+                cout<<"0111";
+                break;
+            case '8':
+                cout<<"1000";
+                break;
+            case '9':
+                cout<<"1001";
+                break;
+            case 'A':
+            case 'a':
+                cout<<"1010";
+                break;
+            case 'B':
+            case 'b':
+                cout<<"1011";
+                break;
+            case 'C':
+            case 'c':
+                cout<<"1100";
+                break;
+            case 'D':
+            case 'd':
+                cout<<"1101";
+                break;
+            case 'E':
+            case 'e':
+                cout<<"1110";
+                break;
+            case 'F':
+            case 'f':
+                cout<<"1111";
+                break;
+            default:
+                cout<<"--Invalid Hex Digit ("<<NumYouEnter[i]<<")--";
+        }
+        i++;
     }
-    
-    cout << "binary value = " << bin << "\n";
+    cout<<endl;
 }
-
-
 
 void BaseEightToTwo(int NumYouEnter){
     
     string bin{};
     
-    while( NumYouEnter > 0 ) {
-        if( NumYouEnter % 8 == 0 ) bin.insert( bin.begin( ), '0' );
+    while( NumYouEnter > 0 && NumYouEnter < 8) {
+        if( NumYouEnter % 10 == 0 ) bin.insert( bin.begin( ), '0' );
         else bin.insert( bin.begin( ), '1' );
         
         NumYouEnter >>= 1;
     }
     
-    cout << "binary value = " << bin << "\n";
+    cout << "\nbinary value = " << bin << "\n";
 }
 
 // Main function
@@ -54,6 +109,7 @@ int main(){
     bool run = true;
     string choice,continuechoice;
     int inputedvalue, count=0;
+	char inputedvaluehex[10];
 
     while(run){
         cout << "What base do you want to convert?\n choices: \n 'hex' | 'dec' | 'oct'\n";
@@ -88,8 +144,8 @@ int main(){
         if(choice == "hex"){
             
             cout << "Enter the Hexadecimal value you want to convert: ";
-            cin >> inputedvalue;
-            BaseSixteenToTwo(inputedvalue);
+            cin >> inputedvaluehex;
+            BaseSixteenToTwo(inputedvaluehex);
 
             //asking if you want to retry or not
             cout << "Do you want to continue? | 'yes' or 'no' |\n\n";
